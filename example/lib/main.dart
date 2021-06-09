9f7ab6400b8630d316b2e5b7517e2da3676e5bf3
shirne <shirne@126.com>
2021-06-09 11:52:52 +0800

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,17 @@
-import 'package:number_stepper/number_stepper.dart';
+
 import 'package:flutter/material.dart';
+
+import 'package:cart_stepper/stepper.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(
@@ -30,16 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key,required this.title}) : super(key: key);
 
   final String title;
 
@@ -54,7 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
@@ -78,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Center(
-              child: NumberStepper(
+              child: CartStepper(
                 count: _counterInit,
                 didChangeCount: (count) {
                   setState(() {
@@ -96,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Center(
-              child: NumberStepper(
+              child: CartStepper(
                 count: _counter,
                 axis: Axis.vertical,
                 didChangeCount: (count) {
@@ -119,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Center(
-                    child: NumberStepper(
+                    child: CartStepper(
                       count: _counter,
                       size: 20,
                       didChangeCount: (count) {
@@ -133,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Center(
-                    child: NumberStepper(
+                    child: CartStepper(
                       count: _counter,
                       size: 30,
                       deActiveForegroundColor: Colors.red,
@@ -150,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Center(
-                    child: NumberStepper(
+                    child: CartStepper(
                       count: _counter,
                       size: 80,
                       didChangeCount: (count) {
@@ -172,14 +154,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Center(
-              child: NumberStepper(
+              child: CartStepper(
                 count: _counterLimit,
                 size: 30,
                 didChangeCount: (count) {
                   if(count < 1){
-                    Scaffold.of(context).showSnackBar(
-                      SnackBar(content: Text('数量不能小于 0 哦~'),backgroundColor: Colors.orangeAccent,)
-                    );
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('数量不能小于 1 哦~'),backgroundColor: Colors.orangeAccent,));
                     return;
                   }
                   setState(() {
