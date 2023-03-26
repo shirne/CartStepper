dcda109ab6dcc8253d0d452a412453c84389aa58
Dada878 <37009584+dada878@users.noreply.github.com>
2023-03-26 21:56:45 +0800

    set keyboard type for EditableText

--- lib/src/cart_stepper.dart
+++ lib/src/cart_stepper.dart
@@ -178,6 +178,7 @@ class _CartStepperState<VM extends num> extends State<CartStepper<VM>> {
                     controller: _controller,
                     focusNode: _focusNode,
                     textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
                     style: textStyle,
                     cursorColor: style.activeForegroundColor,
                     backgroundCursorColor: style.activeBackgroundColor,
